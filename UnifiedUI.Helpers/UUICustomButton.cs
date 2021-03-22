9f2e8b8a157408b58de5d3687f1d433ff74cda92
kian.zarrin <kian.zarrin@gmail.com>
2021-03-22 07:35:52 +0200

    doc

--- UnifiedUI.Helpers/UUICustomButton.cs
+++ UnifiedUI.Helpers/UUICustomButton.cs
@@ -6,10 +6,16 @@
     public class UUICustomButton {
         internal UUICustomButton(UIComponent component) => Button = component;
 
+        /// <summary>
+        /// UUI button. you can hide this if necessary.
+        /// </summary>
         public UIComponent Button { get; private set; }
 
         PropertyInfo pIsActive_ => Button.GetType().GetProperty("IsActive");
 
+        /// <summary>
+        /// Use this to change buttons state when mod is closed externally (without clicking on the UUI button).
+        /// </summary>
         public bool IsPressed {
             get => (bool)pIsActive_.GetValue(Button, null);
             set => pIsActive_.SetValue(Button, value, null);
--- UnifiedUI.Helpers/UUIHelpers.cs
+++ UnifiedUI.Helpers/UUIHelpers.cs
@@ -57,6 +57,14 @@
         internal delegate UIComponent RegisterHandlerTool
             (string name, string groupName, string tooltip, string spritefile, ToolBase tool); 
 
+        /// <summary>
+        /// register a button to tie to the given tool.
+        /// </summary>
+        /// <param name="name">game object name for button</param>
+        /// <param name="groupName">the group under which button will be added. use null to addd to the default gorup.</param>
+        /// <param name="spritefile">full path to the file that contains 4 40x40x button sprites(see example)</param>
+        /// <param name="tool">the tool to tie the buttont to.</param>
+        /// <returns>component containing the button. you can hide this component if necessary.</returns>
         public static UIComponent RegisterToolButton(
             string name, string groupName, string tooltip, string spritefile, ToolBase tool) {
             if (!IsUUIEnabled()) return null;
@@ -64,6 +72,15 @@
             return Register(name, groupName, spritefile, tooltip, tool);
         }
 
+        /// <summary>
+        /// register a button to add to UUI.
+        /// </summary>
+        /// <param name="name">game object name for button</param>
+        /// <param name="groupName">the group under which button will be added. use null to addd to the default gorup.</param>
+        /// <param name="spritefile">full path to the file that contains 4 40x40x button sprites(see example)</param>
+        /// <param name="onToggle">call-back for when the button is activated/deactivated</param>
+        /// <param name="onToolChanged">call-back for when any active tool changes.</param>
+        /// <returns>wrapper for the button which you can use to change the its state.</returns>
         public static UUICustomButton RegisterCustomButton(
             string name, string groupName, string tooltip, string spritefile, Action<bool> onToggle, Action<ToolBase> onToolChanged = null) {
             if (!IsUUIEnabled()) return null;
@@ -72,11 +89,25 @@
             return new UUICustomButton(compoennt);
         }
 
+        /// <summary>
+        /// Destroy all gameObjects, components, and children
+        /// </summary>
         public static void Destroy(this UIComponent button) => GameObject.Destroy(button?.gameObject);
 
+        /// <summary>
+        /// Gets the path to the mod that has the user mod instance.
+        /// </summary>
+        /// <param name="userModInstance">instance of IUserMod</param>
+        /// <returns>path to mod</returns>
         public static string GetModPath(this IUserMod userModInstance) =>
             Plugins.FirstOrDefault(p => p?.userModInstance == userModInstance)?.modPath;
 
+        /// <summary>
+        /// Gets the full path to a file from the input mod
+        /// </summary>
+        /// <param name="userModInstance">instance of IUserMod</param>
+        /// <param name="paths">diretory,file names to combine</param>
+        /// <returns>full path to file.</returns>
         public static string GetFullPath(this IUserMod userModInstance, params string[] paths) {
             string ret = userModInstance.GetModPath();
             foreach(string path in paths)
@@ -84,6 +115,9 @@
             return ret;
         }
 
+        /// <summary>
+        /// test if UnifiedUI is present and enable.
+        /// </summary>
         public static bool IsUUIEnabled() {
             var uui = GetUUIPlugin();
             return uui != null && uui.isEnabled;
