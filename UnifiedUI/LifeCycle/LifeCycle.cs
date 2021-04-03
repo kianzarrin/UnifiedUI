af26e2136c7a18c58b428de68608bc712e4e8043
kian.zarrin <kian.zarrin@gmail.com>
2021-04-03 21:52:05 +0300

    simplified lifecylce

--- UnifiedUI/LifeCycle/LifeCycle.cs
+++ UnifiedUI/LifeCycle/LifeCycle.cs
@@ -1,46 +1,22 @@
 namespace UnifiedUI.LifeCycle {
-    //using CitiesHarmony.API;
     using ColossalFramework.UI;
-    using ICities;
     using KianCommons;
+    using KianCommons.IImplict;
     using System.Diagnostics;
     using UnifiedUI.GUI;
     using KianCommons.Plugins;
-    using UnityEngine;
-    using UnityEngine.SceneManagement;
     using UnifiedUI.GUI.ModButtons;
+    using static KianCommons.ReflectionHelpers;
 
-    public static class LifeCycle {
-        public static string HARMONY_ID = "CS.Kian.UnifiedUI";
 
-        public static SimulationManager.UpdateMode UpdateMode => SimulationManager.instance.m_metaData.m_updateMode;
-        public static LoadMode Mode => (LoadMode)UpdateMode;
-        public static string Scene => SceneManager.GetActiveScene().name;
+    internal class LifeCycle : LifeCycleBase, IModWithSettings {
+        public override string Name => "Unified UI " + VersionString;
+        public override string Description => "organsized UI for some other mods into one unified place.";
 
-        public static void Enable() {
-            Log.Debug("Testing StackTrace:\n" + new StackTrace(true).ToString(), copyToGameLog: false);
+        public override void Load() {
+            LogCalled();
             KianCommons.UI.TextureUtil.EmbededResources = false;
             Log.VERBOSE = false;
-
-            if(HelpersExtensions.InGameOrEditor)
-                HotReload();
-        }
-
-        public static void Disable() {
-            Release(); // in case of hot unload
-        }
-
-        public static void OnLevelUnloading() {
-            Release(); // called when loading new game or exiting to main menu.
-        }
-
-        public static void HotReload() {
-            Load();
-        }
-
-        public static void Load() {
-            Log.Info("LifeCycle.Load() called");
-            PluginUtil.Init();
 
             if(PluginUtil.GetNetworkDetective().IsActive())
                 MainPanel.Instance.AddButton<NetworkDetectiveButton>();
@@ -56,7 +32,15 @@ namespace UnifiedUI.LifeCycle {
 
             if(PluginUtil.GetNodeController().IsActive())
                 MainPanel.Instance.AddButton<NodeControllerButton>();
+
         }
+
+        public override void UnLoad() {
+            LogCalled();
+            MainPanel.Release();
+            UUISettings.ReviveDisabledKeys();
+        }
+
 
         public static bool HasMainPanel => UIView.GetAView().GetComponentInChildren<MainPanel>();
 
@@ -65,5 +49,7 @@ namespace UnifiedUI.LifeCycle {
             MainPanel.Release();
             UUISettings.ReviveDisabledKeys();
         }
+
+        public void OnSettingsUI(UIHelper helper) => UUISettings.OnSettingsUI(helper);
     }
 }
